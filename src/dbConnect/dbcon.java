6da50dec1dfd4a1ff2a832cfa85f5c7a518ba2b3
Unknown <psingarathnam@gmail.com>
2017-08-21 10:26:21 +0100

    inventory

---
+++ src/dbConnect/dbcon.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbConnect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Dharshika
+ */
+public class dbcon {
+ public static Connection connect(){
+    Connection conn = null;
+        
+        try{
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = (com.mysql.jdbc.Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/ITP", "Dharshika", "indumathi9@");
+            
+        }
+        catch (Exception e) {
+            
+            System.out.println(e);
+        }
+        
+        
+        
+        return conn;
+    
+ }
+}
